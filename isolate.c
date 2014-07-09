33a3e78cba6ded70aad5ceb55af2e7ecfcf236c7
Martin Mares <mj@ucw.cz>
2014-07-09 11:51:07 +0200

    Updated Copyright years

--- isolate.c
+++ isolate.c
@@ -1,8 +1,8 @@
 /*
  *	A Process Isolator based on Linux Containers
  *
- *	(c) 2012-2013 Martin Mares <mj@ucw.cz>
- *	(c) 2012-2013 Bernard Blackham <bernard@blackham.com.au>
+ *	(c) 2012-2014 Martin Mares <mj@ucw.cz>
+ *	(c) 2012-2014 Bernard Blackham <bernard@blackham.com.au>
  */
 
 #define _GNU_SOURCE
