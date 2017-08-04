ioi/isolate

commit 0e42f1b1ada17dc32a1455435827a67f146c91fd

When closing file descriptors, do not close the meta-file

util.c
@@ -112,6 +112,8 @@ chowntree(char *path, uid_t uid, gid_t gid)
   nftw(path, chowntree_helper, 32, FTW_MOUNT | FTW_PHYS);
 }
 
+static int fd_to_keep = -1;
+
 void
 close_all_fds(void)
 {
@@ -129,7 +131,7 @@ close_all_fds(void)
       long int fd = strtol(e->d_name, &end, 10);
       if (*end)
 	continue;
-      if (fd >= 0 && fd <= 2 || fd == dir_fd)
+      if (fd >= 0 && fd <= 2 || fd == dir_fd || fd == fd_to_keep)
 	continue;
       close(fd);
     }
@@ -152,6 +154,7 @@ meta_open(const char *name)
   if (setfsuid(getuid()) < 0)
     die("Failed to switch FS UID: %m");
   metafile = fopen(name, "w");
+  fd_to_keep = fileno(metafile);
   if (setfsuid(geteuid()) < 0)
     die("Failed to switch FS UID back: %m");
   if (!metafile)

commit f8acf050c76d227cb447517e55cf8b242ce05f1f

Fixed a potential NULL pointer dereference
Bug introduced in 0e42f1b1ada17dc32a1455435827a67f146c91fd.

util.c
@@ -154,11 +154,11 @@ meta_open(const char *name)
   if (setfsuid(getuid()) < 0)
     die("Failed to switch FS UID: %m");
   metafile = fopen(name, "w");
-  fd_to_keep = fileno(metafile);
   if (setfsuid(geteuid()) < 0)
     die("Failed to switch FS UID back: %m");
   if (!metafile)
     die("Failed to open metafile '%s'",name);
+  fd_to_keep = fileno(metafile);
 }
 
 void