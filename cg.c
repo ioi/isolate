dbb6fbed95d33e7b87274fb03c29d2bb52c3788c
Antoine Pietri <antoine.pietri1@gmail.com>
2018-03-03 18:06:51 +0100

    cg: add cg-oom-killed meta entry
    It is hard to judge solely from the meta output whether a program has
    been SIGKILL by the OOM killer or something else.
    
    The cgroup API provides us with a memory.oom_control file that contains
    a value "oom_kill" which contains the number of times the cgroup has
    received an OOM kill. If this value is greater than 0, we report that
    there was an OOM kill in the meta file with the cg-oom-killed entry.

--- cg.c
+++ cg.c
@@ -280,6 +280,17 @@ cg_stats(void)
     }
   if (mem)
     meta_printf("cg-mem:%lld\n", mem >> 10);
+
+  // OOM kill detection
+  if (cg_read(CG_MEMORY, "?memory.oom_control", buf))
+    {
+      int oom_killed = 0;
+      const char *oom_kill_line = strstr(buf, "oom_kill ");
+
+      if (oom_kill_line && sscanf(oom_kill_line, "oom_kill %d", &oom_killed))
+        if (oom_killed)
+          meta_printf("cg-oom-killed:1\n");
+    }
 }
 
 void
