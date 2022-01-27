4cc2e1b823a45a6e556ae083bc2f1c55b4f9309f
Martin Mares <mj@ucw.cz>
2022-01-27 18:21:21 +0100

    Replaced nftw() by our directory tree walker
    This stops potential symlink swapping attacks by the caller of isolate.

--- util.c
+++ util.c
@@ -1,15 +1,17 @@
 /*
  *	Process Isolator -- Utility Functions
  *
- *	(c) 2012-2017 Martin Mares <mj@ucw.cz>
+ *	(c) 2012-2022 Martin Mares <mj@ucw.cz>
  *	(c) 2012-2014 Bernard Blackham <bernard@blackham.com.au>
  */
 
 #include "isolate.h"
 
+#include <assert.h>
 #include <dirent.h>
 #include <errno.h>
-#include <ftw.h>
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -70,39 +72,97 @@ make_dir(char *path)
     die("Cannot create %s: already exists, but not a directory", path);
 }
 
+/*
+ *  Once upon a time, we used nftw() for traversing directory trees.
+ *  It was simple, but unfortunately prone to symlink swapping attacks.
+ *  Using FTW_CHDIR would prevent the attacks, but it interacts badly with
+ *  FTW_DEPTH which we need when removing directory trees. See bug report at
+ *  https://sourceware.org/bugzilla/show_bug.cgi?id=28831.
+ *
+ *  We therefore switched to our implementation based on using openat(),
+ *  fstatat() and similar functions.
+ */
 
-static int
-rmtree_helper(const char *fpath, const struct stat *sb, int typeflag UNUSED, struct FTW *ftwbuf UNUSED)
+static void
+walktree_fd(int dir_fd, dev_t root_dev, void (*callback)(int dir_fd, const char *name, bool is_dir))
 {
-  if (S_ISDIR(sb->st_mode))
+  DIR *dir = fdopendir(dir_fd);
+  if (!dir)
+    die("fdopendir failed: %m");
+
+  struct dirent *de;
+  while (de = readdir(dir))
     {
-      if (rmdir(fpath) < 0)
-	die("Cannot rmdir %s: %m", fpath);
+      if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, ".."))
+	continue;
+
+      struct stat st;
+      if (fstatat(dir_fd, de->d_name, &st, AT_SYMLINK_NOFOLLOW) < 0)
+	die("Cannot stat %s: %m", de->d_name);
+
+      if (st.st_dev != root_dev)
+	die("Unexpected mountpoint: %s", de->d_name);
+
+      if (S_ISDIR(st.st_mode))
+	{
+	  int fd = openat(dir_fd, de->d_name, O_RDONLY | O_DIRECTORY | O_NOFOLLOW);
+	  if (fd < 0)
+	    die("Cannot open directory %s: %m", de->d_name);
+	  walktree_fd(fd, root_dev, callback);;
+	  callback(dir_fd, de->d_name, 1);
+	}
+      else
+	callback(dir_fd, de->d_name, 0);
+    }
+
+  closedir(dir);
+}
+
+static void
+walktree(const char *path, void (*callback)(int dir_fd, const char *name, bool is_dir))
+{
+  int fd = open(path, O_RDONLY | O_DIRECTORY);
+  if (fd < 0)
+    die("Cannot open directory %s: %m", path);
+
+  struct stat st;
+  if (fstat(fd, &st) < 0)
+    die("Cannot stat %s: %m", path);
+  assert(S_ISDIR(st.st_mode));
+
+  walktree_fd(fd, st.st_dev, callback);
+  callback(AT_FDCWD, path, 1);
+}
+
+static void
+rmtree_helper(int dir_fd, const char *name, bool is_dir)
+{
+  if (is_dir)
+    {
+      if (unlinkat(dir_fd, name, AT_REMOVEDIR) < 0)
+	die("Cannot rmdir %s: %m", name);
     }
   else
     {
-      if (unlink(fpath) < 0)
-	die("Cannot unlink %s: %m", fpath);
+      if (unlinkat(dir_fd, name, 0) < 0)
+	die("Cannot unlink %s: %m", name);
     }
-  return 0;
 }
 
 void
 rmtree(char *path)
 {
-  nftw(path, rmtree_helper, 32, FTW_MOUNT | FTW_PHYS | FTW_DEPTH);
+  walktree(path, rmtree_helper);
 }
 
 static uid_t chown_uid;
 static gid_t chown_gid;
 
-static int
-chowntree_helper(const char *fpath, const struct stat *sb UNUSED, int typeflag UNUSED, struct FTW *ftwbuf UNUSED)
+static void
+chowntree_helper(int dir_fd, const char *name, bool is_dir UNUSED)
 {
-  if (lchown(fpath, chown_uid, chown_gid) < 0)
-    die("Cannot chown %s: %m", fpath);
-  else
-    return 0;
+  if (fchownat(dir_fd, name, chown_uid, chown_gid, AT_SYMLINK_NOFOLLOW) < 0)
+    die("Cannot chown %s: %m", name);
 }
 
 void
@@ -110,7 +170,7 @@ chowntree(char *path, uid_t uid, gid_t gid)
 {
   chown_uid = uid;
   chown_gid = gid;
-  nftw(path, chowntree_helper, 32, FTW_MOUNT | FTW_PHYS);
+  walktree(path, chowntree_helper);
 }
 
 static int fd_to_keep = -1;
