cecc9c9cfd1381600755b481ecacb981623902cf
Martin Mares <mj@ucw.cz>
2023-02-23 18:38:32 +0100

    Implement locking of sandboxes
    First, multiple instances of Isolate are not allowed to run on the
    same sandbox.
    
    Second, once a user initializes a sandbox, it cannot be used by
    other users until it is cleaned up.

--- isolate.c
+++ isolate.c
@@ -7,6 +7,7 @@
 
 #include "isolate.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
@@ -16,6 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <net/if.h>
+#include <sys/file.h>
 #include <sys/mount.h>
 #include <sys/resource.h>
 #include <sys/signal.h>
@@ -87,6 +89,7 @@ static int inherit_fds;
 static int default_dirs = 1;
 static int tty_hack;
 static bool special_files;
+static bool wait_if_busy;
 
 int cg_enable;
 int cg_memory_limit;
@@ -117,6 +120,126 @@ static int status_pipes[2];
 
 static int get_wall_time_ms(void);
 static int get_run_time_ms(struct rusage *rus);
+
+/*** Locks ***/
+
+/*
+ *  Whenever a sandbox is initialized, a lock file is created, which
+ *  records which user owns the sandbox and whether the cgroup mode is used.
+ *  Atempts to use the same sandbox by a different user are refused.
+ *
+ *  The lock file is locked whenever Isolate runs in that sandbox.
+ */
+
+#define LOCK_MAGIC 0x48736f6c
+
+struct lock_record {
+  uint32_t magic;
+  uint32_t owner_uid;
+  unsigned char cg_enabled;
+  unsigned char is_initialized;
+  unsigned char rfu[2];
+};
+
+static int lock_fd = -1;
+static struct lock_record lock;
+
+static void
+lock_write(void)
+{
+  int n = pwrite(lock_fd, &lock, sizeof(lock), 0);
+  if (n != sizeof(lock))
+    die("Cannot write lock file: %m");
+}
+
+static bool
+lock_box(bool is_init)
+{
+  if (!dir_exists(cf_lock_root))
+    make_dir(cf_lock_root);
+
+  char lock_name[256];
+  int name_len = snprintf(lock_name, sizeof(lock_name), "%s/%d", cf_lock_root, box_id);
+  assert(name_len < (int) sizeof(lock_name));
+
+  lock_fd = open(lock_name, O_RDWR | (is_init ? O_CREAT : 0), 0666);
+  if (lock_fd < 0)
+    {
+      if (errno == ENOENT)
+	return false;
+      die("Cannot open %s: %m", lock_name);
+    }
+
+  if (flock(lock_fd, LOCK_EX | (wait_if_busy ? 0 : LOCK_NB)) < 0)
+    {
+      if (errno == EWOULDBLOCK)
+	die("This box is currently in use by another process");
+      die("Cannot lock %s: %m", lock_name);
+    }
+
+  int n = read(lock_fd, &lock, sizeof(lock));
+  if (n < 0)
+    die("Cannot read %s: %m");
+
+  if (n > 0)
+    {
+      if (n != sizeof(lock) || lock.magic != LOCK_MAGIC)
+	die("Lock file %s has incompatible format", lock_name);
+      if (lock.is_initialized && lock.owner_uid != orig_uid && orig_uid != 0)
+	die("This box belongs to a different user (uid %d)", lock.owner_uid);
+      if (lock.cg_enabled != cg_enable)
+	die("This box was initialized with an incompatible control group mode");
+    }
+
+  if (is_init)
+    {
+      lock.magic = LOCK_MAGIC;
+      lock.owner_uid = orig_uid;
+      lock.cg_enabled = cg_enable;
+      lock.is_initialized = 0;
+      lock_write();
+      return true;
+    }
+  else
+    {
+      if (n > 0)
+	{
+	  if (!lock.is_initialized)
+	    die("This box was not initialized properly");
+	  return true;
+	}
+      else
+	{
+	  // This means that somebody else is just creating the sandbox and we locked it
+	  // between his creation of the lock file and locking it.
+	  return false;
+	}
+    }
+
+  // The acquired lock will be automatically released on process exit.
+}
+
+static void
+lock_close(void)
+{
+  if (lock_fd >= 0)
+    {
+      close(lock_fd);
+      lock_fd = -1;
+    }
+}
+
+static void
+lock_remove(void)
+{
+  // To avoid race conditions, we must never unlink lock files.
+  // We just truncate them to zero length.
+  assert(lock_fd >= 0);
+  if (ftruncate(lock_fd, 0) < 0)
+    die("Cannot truncate lock file: %m");
+  close(lock_fd);
+  lock_fd = -1;
+}
 
 /*** Messages and exits ***/
 
@@ -734,6 +857,7 @@ box_proxy(void *arg)
   close(error_pipes[0]);
   close(status_pipes[0]);
   meta_close();
+  lock_close();
   reset_signals();
 
   pid_t inside_pid = fork();
@@ -770,9 +894,6 @@ box_init(void)
   box_gid = cf_first_gid + box_id;
 
   snprintf(box_dir, sizeof(box_dir), "%s/%d", cf_box_root, box_id);
-  make_dir(box_dir);
-  if (chdir(box_dir) < 0)
-    die("chdir(%s): %m", box_dir);
 }
 
 /*** Commands ***/
@@ -786,20 +907,25 @@ self_name(void)
 static void
 do_cleanup(void)
 {
-  rmtree(box_dir);
+  if (dir_exists(box_dir))
+    {
+      msg("Removing box directory\n");
+      rmtree(box_dir);
+    }
   cg_remove();
 }
 
 static void
 init(void)
 {
-  if (dir_exists("box"))
-    {
-      msg("Deleting previous sandbox\n");
-      do_cleanup();
-    }
+  lock_box(true);
+
+  do_cleanup();
 
   msg("Preparing sandbox\n");
+  make_dir(box_dir);
+  if (chdir(box_dir) < 0)
+    die("chdir(%s): %m", box_dir);
   if (mkdir("box", 0700) < 0)
     die("Cannot create box: %m");
   if (chown("box", orig_uid, orig_gid) < 0)
@@ -808,19 +934,23 @@ init(void)
   cg_prepare();
   set_quota();
 
+  lock.is_initialized = 1;
+  lock_write();
+
   puts(box_dir);
 }
 
 static void
 cleanup(void)
 {
-  if (dir_exists("box"))
+  if (!lock_box(false))
+    msg("Nothing to do -- box did not exist\n");
+  else
     {
       msg("Deleting sandbox\n");
       do_cleanup();
+      lock_remove();
     }
-  else
-    msg("Nothing to do -- box directory did not exist\n");
 }
 
 static void
@@ -872,11 +1002,17 @@ find_box_pid(void)
 static void
 run(char **argv)
 {
-  if (!dir_exists("box"))
-    die("Box directory not found, did you run `%s --init'?", self_name());
+  if (!lock_box(false))
+    die("Box not found, did you run `%s --init'?", self_name());
+
+  if (chdir(box_dir) < 0)
+    die("chdir(%s): %m", box_dir);
 
   if (!inherit_fds)
-    close_all_fds();
+    {
+      keep_fd(lock_fd);
+      close_all_fds();
+    }
 
   chowntree("box", box_uid, box_gid, false);
   cleanup_ownership = 1;
@@ -968,6 +1104,7 @@ Options:\n\
 -t, --time=<time>\tSet run time limit (seconds, fractions allowed)\n\
     --tty-hack\t\tAllow interactive programs in the sandbox (see man for caveats)\n\
 -v, --verbose\t\tBe verbose (use multiple times for even more verbosity)\n\
+    --wait\t\tIf the sandbox is currently busy, wait instead of refusing to run\n\
 -w, --wall-time=<time>\tSet wall clock time limit (seconds, fractions allowed)\n\
 \n\
 Commands:\n\
@@ -992,6 +1129,7 @@ enum opt_code {
   OPT_TTY_HACK,
   OPT_CORE,
   OPT_SPECIAL_FILES,
+  OPT_WAIT,
 };
 
 static const char short_opts[] = "b:c:d:DeE:f:i:k:m:M:o:p::q:r:st:vw:x:";
@@ -1029,6 +1167,7 @@ static const struct option long_opts[] = {
   { "tty-hack",		0, NULL, OPT_TTY_HACK },
   { "verbose",		0, NULL, 'v' },
   { "version",		0, NULL, OPT_VERSION },
+  { "wait",		0, NULL, OPT_WAIT },
   { "wall-time",	1, NULL, 'w' },
   { NULL,		0, NULL, 0 }
 };
@@ -1168,6 +1307,9 @@ main(int argc, char **argv)
 	break;
       case OPT_SPECIAL_FILES:
 	special_files = true;
+	break;
+      case OPT_WAIT:
+	wait_if_busy = true;
 	break;
       default:
 	usage(NULL);
