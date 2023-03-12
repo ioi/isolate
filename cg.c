ioi/isolate

commit 795c8ff990b21400a877181dbf7a86585105f19b

changing the order of setting limit_in_bytes

cg.c
@@ -238,8 +238,8 @@ cg_enter(void)
 
   if (cg_memory_limit)
     {
-      cg_write(CG_MEMORY, "memory.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
       cg_write(CG_MEMORY, "?memory.memsw.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
+      cg_write(CG_MEMORY, "memory.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
       cg_write(CG_MEMORY, "memory.max_usage_in_bytes", "0\n");
       cg_write(CG_MEMORY, "?memory.memsw.max_usage_in_bytes", "0\n");
     }

commit 77fa9a4a86c40dd5bb3d27e62dbd0799caa837a1

Fix cgroup memory limits not working
It broke from changes in #109, as memory.memsw.limit_in_bytes
cannot be set before memory.limit_in_bytes.

cg.c
@@ -238,8 +238,12 @@ cg_enter(void)
 
   if (cg_memory_limit)
     {
-      cg_write(CG_MEMORY, "?memory.memsw.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
+      // Reset any existing limit on memory.memsw.limit_in_bytes,
+      // as memory.limit_in_bytes cannot be set higher than it.
+      cg_write(CG_MEMORY, "?memory.memsw.limit_in_bytes", "-1\n");
+
       cg_write(CG_MEMORY, "memory.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
+      cg_write(CG_MEMORY, "?memory.memsw.limit_in_bytes", "%lld\n", (long long) cg_memory_limit << 10);
       cg_write(CG_MEMORY, "memory.max_usage_in_bytes", "0\n");
       cg_write(CG_MEMORY, "?memory.memsw.max_usage_in_bytes", "0\n");
     }