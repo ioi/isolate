45787bd6d98ac89205cabfb2206c44436f125aa1
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-04-20 16:34:44 +0700

    fix: remove later check since it will always fail without isolate-cg-keeper

--- docker/start_isolate.sh
+++ docker/start_isolate.sh
@@ -3,21 +3,17 @@
 set -e
 
 QUIET=true
-SKIP_CHECK=true
 ISOLATE_CHECK_EXECUTE=false
 for arg in "$@"; do
     if [ "$arg" == "--help" ]; then
         echo "$(basename "$0")"
-        echo "Usage: [--verbose] [--strict-check] [--execute-patches] [--help]"
+        echo "Usage: [--verbose] [--execute-patches] [--help]"
         echo "  --verbose: Print every thing"
-        echo "  --strict-check: Fail if patches not applied properly."
         echo "  --execute-patches: Run isolate-check-environment --execute --quiet. Increases reproducibility."
         echo "  --help: Show this help message"
         break
     elif [ "$arg" == "--verbose" ]; then
         QUIET=false
-    elif [ "$arg" == "--strict-check" ]; then
-        SKIP_CHECK=false
     elif [ "$arg" == "--execute-patches" ]; then
         ISOLATE_CHECK_EXECUTE=true
     fi
@@ -30,22 +26,15 @@ print() {
 }
 
 if [ $ISOLATE_CHECK_EXECUTE = true ]; then
-    print "Running isolate-check-environment --execute --quiet"
-    isolate-check-environment --execute --quiet > /dev/null 2> /dev/null || true
-fi
-
-if [ $SKIP_CHECK = true ]; then
-    print "Skipping check"
-else
-    print "Rechecking environment. Fail if not properly set up."
-    if [ $QUIET = true ]; then
-        isolate-check-environment --quiet > /dev/null 2> /dev/null
+    if [ $QUIET = false ]; then
+        print "Running isolate-check-environment --execute"
+        isolate-check-environment --execute || true
     else
-        isolate-check-environment
+        print "Running isolate-check-environment --execute --quiet"
+        isolate-check-environment --execute --quiet > /dev/null 2> /dev/null || true
     fi
-    print "Environment is properly set up."
 fi
 
 # Run isolate daemon
 print "Running isolate daemon"
-exec isolate-cg-keeper
+nohup isolate-cg-keeper
